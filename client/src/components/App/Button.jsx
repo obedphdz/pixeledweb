ca206105cfab6bddc10bd16ca62d152308480fc3
obedphdz <bedphdz@gmail.com>
2023-10-12 09:08:18 -0600

    Edicion colores a degradados pixeled y botones, cambio de primary color azul al rosa

--- client/src/components/App/Button.jsx
+++ client/src/components/App/Button.jsx
@@ -5,6 +5,7 @@ const Button = ({ styles }) => (
 		<button
 			type='button'
 			className={`py-4 px-6 font-poppins font-medium text-[18px] text-whiteColor bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+			style={{ background: 'linear-gradient(to right, #e62657, #280028)' }}
 		>
 			Iniciar Pedido
 		</button>
