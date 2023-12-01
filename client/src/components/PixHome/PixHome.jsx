bae02cbbeb8deecf9546d0389a3441c1af97fcb7
obedphdz <bedphdz@gmail.com>
2023-12-01 11:11:22 -0600

    PixHome rutas correjidas

--- client/src/components/PixHome/PixHome.jsx
+++ client/src/components/PixHome/PixHome.jsx
@@ -1,10 +1,9 @@
-
-import Admin  from '../AppAdmin/Admin'
-import ErrorPage from '../ErrorPage/ErrorPage'
+import Admin from '../AppAdmin/Admin';
+import ErrorPage from '../ErrorPage/ErrorPage';
 import Login from '../Login/Login';
-import Register from '../Register/Register'
-import NuevaOrden from '../AppAdmin/NuevaOrden/NuevaOrden'
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Register from '../Register/Register';
+import NuevaOrden from '../AppAdmin/NuevaOrden/NuevaOrden';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from '../App/App';
 import CrearCotizaciones from '../AppAdmin/CrearCotizaciones/CrearCotizaciones';
 import Precios from '../AppAdmin/PreciosLista/Precios';
@@ -24,46 +23,57 @@ import HistorialJefeDise from '../AppAdmin/AppJefeDiseñador/HistorialPedidos/Hi
 import VistaAsigPedi from '../AppAdmin/AppJefeDiseñador/AsignacionPedidos/VistaAsigPedi';
 import MisPedidos from '../AppAdmin/AppJefeDiseñador/Pedidos Asignados/MisPedidos';
 
-
-
 function PixHome() {
-    return (	
+	return (
+		<div>
+			<Router>
+				<Routes>
+					{/* <Route path="/" element={<><NavBar/><Slider/><Banner/></>}/> */}
+					<Route path='/' element={<App />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/admin' element={<Admin />} />
+					<Route path='/admin/nueva-orden' element={<NuevaOrden />} />
+					<Route path='/admin/precios' element={<Precios />} />
+					<Route path='/admin/crear-cotiza' element={<CrearCotizaciones />} />
+					<Route path='/admin/ordenes' element={<VistaOrdenes />} />
+					<Route
+						path='/admin/ordenes/detalles-ord'
+						element={<VistaDetallesOrd />}
+					/>
+					<Route path='/admin/empleados' element={<VistaEmp />} />
+					<Route path='/admin/historial' element={<VistaHistorial />} />
+					<Route path='/admin/clientes' element={<VistaClientes />} />
 
-      <div >
-        <Router>
-          <Routes>
-            {/* <Route path="/" element={<><NavBar/><Slider/><Banner/></>}/> */}        
-            <Route path="/" element={<App/>}/> 
-            <Route path="/admin" element={<Admin/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-            <Route path='/nueva-orden' element={<NuevaOrden/>}/>
-            <Route path='/precios' element={<Precios/>}/>
-            <Route path='/crear-cotiza' element={<CrearCotizaciones/>}/>
-            <Route path='/cliente' element={<Cliente/>}/>
+					<Route path='/jefe-dise' element={<JefeDise />} />
+					<Route
+						path='/jefe-dise/crear-orden-dise'
+						element={<CrearOrdenDi />}
+					/>
+					<Route path='/jefe-dise/historial' element={<HistorialJefeDise />} />
+					<Route path='/jefe-dise/asignar' element={<VistaAsigPedi />} />
+					<Route
+						path='/jefe-dise/detalles-ord'
+						element={<VistaDetallesDise />}
+					/>
+					<Route path='/jefe-dise/pedidos' element={<MisPedidos />} />
 
-            <Route path='/jefe-dise' element={<JefeDise/>}/>
-            <Route path='/jefe-dise/crear-orden-dise' element={<CrearOrdenDi/>}/>
-            <Route path='/jefe-dise/historial' element={<HistorialJefeDise/>}/>
-            <Route path='/jefe-dise/asignar' element={<VistaAsigPedi/>}/>
-            <Route path='/jefe-dise/detalles-ord' element={<VistaDetallesDise/>}/>
-            <Route path='/jefe-dise/pedidos' element={<MisPedidos/>}/>
+					<Route path='/diseñador' element={<Disenador />} />
+					<Route
+						path='/diseñador/detalles-ord'
+						element={<VistaDetallesDise />}
+					/>
+					<Route path='/diseñador/historial' element={<HistorialDise />} />
+					<Route path='/diseñador/mis-pedidos' element={<MisPedidos />} />
 
-            <Route path='/diseñador' element={<Disenador/>}/>
-            <Route path='/diseñador/detalles-ord' element={<VistaDetallesDise/>}/>
-            <Route path='/diseñador/historial' element={<HistorialDise/>}/>
-            <Route path='/diseñador/mis-pedidos' element={<MisPedidos/>}/>
+					<Route path='/modal' element={<MiModal />} />
+					<Route path='/cliente' element={<Cliente />} />
 
-            <Route path='/modal' element={<MiModal/>}/>
-            <Route path='/cliente' element={<Cliente/>}/>
-
-
-            <Route path='*' element={<ErrorPage/>}/>
-          </Routes>
-        </Router> 
-      </div>
+					<Route path='*' element={<ErrorPage />} />
+				</Routes>
+			</Router>
+		</div>
 	);
 }
 
 export default PixHome;
-
